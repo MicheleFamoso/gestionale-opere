934fa3f85044eb141a24e76174688bb24b07bb19
Vgest <michelefamoso@gmail.com>
2026-02-24 14:37:33 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200', // o la porta del frontend
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Rimuove proprietà non definite nel DTO

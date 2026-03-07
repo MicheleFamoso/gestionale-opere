6a5c8625b38c935d9d39ad173950d0cfab3b4786
MicheleFamoso <michelefamoso@gmail.com>
2026-03-07 14:58:12 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200', // o la porta del frontend
+    origin: 'http://localhost:5173', // o la porta del frontend
   });
   app.useGlobalPipes(
     new ValidationPipe({
